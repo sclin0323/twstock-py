0c1b5aaecb7a1385b413fb1ca65c741aeb6c4433
sclin0323 <sclin0323@gmail.com>
2024-03-14 12:50:02 +0800

    schedule

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Hello Python!!');
+import schedule
+import time
+
+def job():
+    print("定期执行的任务")
+
+# 设置每1分钟执行一次任务
+schedule.every(1).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
